5d55842355c276e9ad05b1d037fa95c3d81e68b2
tkmk96 <tkmk96@gmail.com>
2018-09-25 20:59:18 +0200

    01 Composer command done

--- 01/stat.py
+++ 01/stat.py
@@ -1,32 +1,46 @@
 import sys
 import re
 
-def addOrCreate(dict, key):
-    if dict.has_key(key):
+
+def add_composer(dict, key):
+    if dict.get(key) is not None:
         dict[key] += 1
     else:
         dict[key] = 1
 
-def findComposers(f):
+
+def find_composers(f):
     r = re.compile(r"Composer: (.*)")
     composers = {}
     for line in f:
         m = r.match(line)
         if m is None: continue
-        addOrCreate(composers, m.group(1))
+        temp_composers = m.group(1).split(';')
+        for composer in temp_composers:
+            composer_name = re.sub(r'\([+\d].*\)', '', composer).strip()
+            if composer_name != "":
+                add_composer(composers, composer_name)
     return composers
 
+
+def print_composers(composers):
+    print(composers.__len__())
+    for composer, count in composers.items():
+        print(composer + ": %d" % count)
+
+
 def composer(f):
-    print findComposers(f)
-    
+    print_composers(find_composers(f))
+
 
 def centuries(f):
-    print "centuries"
+    print("centuries")
+
 
 filename = sys.argv[1]
 method = sys.argv[2]
 
-f = open(filename, 'r')
+f = open(filename, encoding="utf8")
 
 if method == "composer":
     composer(f)
